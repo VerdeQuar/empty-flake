cdcc8b7197da0284eaaf1f0657dd5dfaaf3ead42
VerdeQuar <verdequar@gmail.com>
2024-05-11 13:50:16 +0200

    empty

--- flake.nix
+++ flake.nix
@@ -1,18 +1,6 @@
 {
-  outputs = {pkgs, ...}: {
+  outputs = {...}: {
     overlays = [
-      (pkgs.bitwig-studio.overrideAttrs (oldAttrs: rec {
-        version = "5.0.4";
-        src = fetchurl {
-          url = "https://downloads.bitwig.com/stable/${version}/bitwig-studio-${version}.deb";
-          sha256 = "sha256-IkhUkKO+Ay1WceZNekII6aHLOmgcgGfx0hGo5ldFE5Y=";
-        };
-        postInstall = ''
-          export SOPS_AGE_KEY_FILE="${config.sops.age.keyFile}";
-          rm $out/libexec/bin/bitwig.jar;
-          ${pkgs.sops}/bin/sops -d ${../sops/bin/J8BJw} > $out/libexec/bin/bitwig.jar
-        '';
-      }))
     ];
   };
 }
